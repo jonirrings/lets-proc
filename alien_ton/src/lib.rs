f2fe155d8bee7676dca78856c21374955c508040
Jonir Rings <peterpuyi@live.cn>
2018-06-05 21:32:58 +0800

    alien_ton done

--- alien_ton/src/lib.rs
+++ alien_ton/src/lib.rs
@@ -1,12 +1,32 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate rand;
+
+use rand::prelude::*;
+use std::char;
+
+fn is_printable_ascii(ch: u32) -> Option<char> {
+    if ch <= 0x20u32 || ch >= 0x7eu32 {
+        return None;
     }
+    return char::from_u32(ch);
+}
+
+fn print_alien_ton() {
+    let mut ws = Vec::with_capacity(64);
+    let mut rng = thread_rng();
+    loop {
+        let rand_u32 = rng.next_u32() % 128;
+        match is_printable_ascii(rand_u32) {
+            None => continue,
+            Some(ch) => ws.push(ch),
+        }
+        if ws.len() >= 64 {
+            break;
+        }
+    }
+    println!("Alien Words: {}", ws.iter().collect::<String>())
 }
 
 #[no_mangle]
 pub extern "C" fn plug_task() {
-    println!("Hello from alien_ton");
+    print_alien_ton();
 }
